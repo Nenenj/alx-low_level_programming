8300ff8037812e9cd77f18a6a39aceae983a6a8a
Nenenj <nnennanjoku08@gmail.com>
2022-09-11 16:03:46 -0700

    Task 5 Hello, prints

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
-
 /**
- * main - Printing now
+ * main - Prints a text
  *
- * Return: Always 0 (Success)
+ * Return: Always 0 (Successful)
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art, \n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
