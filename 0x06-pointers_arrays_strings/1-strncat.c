e0b3555b938ec71006fb4b179b086538a35ef556
Nenenj <nnennanjoku08@gmail.com>
2022-09-22 00:55:36 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,22 +1,28 @@
 #include "main.h"
 
 /**
- * _stncat - concatenate two strings but add inputted number of bytes
- * @dest: string to be appended
- * @src: string to be completed at the end of dest
- * @n: integer parameter to compare index to
- * Return: returns new concatenated string
+ * _strncat - concatenates two strings.
+ * @dest: destination
+ * @src: source
+ * @n: amount of bytes used from src.
+ * Return: a pointer to the resulting string dest
  */
-
 char *_strncat(char *dest, char *src, int n)
 {
-	int index = 0, dest_len = 0;
+	int count = 0, count2 = 0;
 
-	while (dest[index++])
-		dest_len++;
-
-	for (index = 0; src[index] && index < n; index++)
-		dest[dest_len++] = src[index];
-
+	while (*(dest + count) != '\0')
+	{
+		count++;
+	}
+	while (count2 < n)
+	{
+		*(dest + count) = *(src + count2);
+		if (*(src + count2) == '\0')
+			break;
+		count++;
+		count2++;
+	}
 	return (dest);
 }
+
