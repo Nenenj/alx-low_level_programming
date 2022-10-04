b1be1c9c3e649664206c2db0491ce8e6e8fc8835
Nenenj <nnennanjoku08@gmail.com>
2022-10-04 14:57:56 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -6,6 +6,7 @@
  * @s2: second string.
  * Return: char pointer
  */
+
 char *str_concat(char *s1, char *s2)
 {
 	char *strout;
@@ -16,8 +17,9 @@ char *str_concat(char *s1, char *s2)
 	if (s2 == NULL)
 		s2 = "";
 	for (i = 0; s1[i] != '\0'; i++)
-		for (j = 0; s2[j] != '\0'; j++)
-			;
+		;
+	for (j = 0; s2[j] != '\0'; j++)
+		;
 	strout = malloc(sizeof(char) * (i + j + 1));
 	if (strout == NULL)
 	{
