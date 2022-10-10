bf557bf6a376b41102cda0af7e9ddf746c32b44f
Nenenj <nnennanjoku08@gmail.com>
2022-10-10 14:36:53 -0700

    0-object_like_macro.h

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,12 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef SIZE
+#define SIZE 1024
 
-#include <stdio.h>
-
-#include "0-object_like_macro.h"
-#include "1-pi.h"
-#include "3-function_like_macro.h"
-#include "4-sum.h"
-
-#endif /* MAIN_H */
+#endif /* size */
 
