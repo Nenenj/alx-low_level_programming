a76f48197b26721f194d2b4ba5c63a520fac993f
Nenenj <nnennanjoku08@gmail.com>
2022-10-13 04:46:18 -0700

    3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -4,6 +4,7 @@
  * main - program that perfroms simple operations
  * @argc: number of arguments
  * @argv: array of arguments
+ *
  * Return: Always 0 (Success)
  */
 int main(int argc, char *argv[])
@@ -17,22 +18,29 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(98);
 	}
+
 	arg1 = atoi(argv[1]);
 	arg2 = atoi(argv[3]);
+
 	func = get_op_func(argv[2]);
+
 	if (!func)
 	{
 		printf("Error\n");
 		exit(99);
 	}
+
 	o = *argv[2];
+
 	if ((o == '/' || o == '%') && arg2 == 0)
 	{
 		printf("Error\n");
 		exit(100);
 	}
+
 	result = func(arg1, arg2);
+
 	printf("%d\n", result);
+
 	return (0);
 }
-
