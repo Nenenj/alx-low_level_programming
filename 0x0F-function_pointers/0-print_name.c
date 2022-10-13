b964aec0662c79f86be31b238252b3e1babbe221
Nenenj <nnennanjoku08@gmail.com>
2022-10-13 00:52:03 -0700

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -3,12 +3,12 @@
 /**
  * print_name - name to be printed
  * @name: name to be printed
- * @f: a pointer to the function that print name
+ * @f: function to print with
+ *
+ * Return: void
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (!name || !f)
-		return;
-	f(name);
+	if (name != NULL && f != NULL)
+		f(name);
 }
-
